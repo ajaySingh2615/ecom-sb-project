9a15df94f5265fb1806b7833f43a80129538a06f
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-09-28 13:18:35 +0530

    Implementing Implement UserDetailsService and UserRepository for user authentication

---
+++ src/main/java/com/ecom/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.ecom.repositories;
+
+import com.ecom.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUserName(String username);
+}
