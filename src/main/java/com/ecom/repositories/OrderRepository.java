7d0638e724e86164b51002214e453bd24b5314d2
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-10-17 02:34:27 +0530

    Setting up repository for managing order

---
+++ src/main/java/com/ecom/repositories/OrderRepository.java
@@ -0,0 +1,11 @@
+package com.ecom.repositories;
+
+import com.ecom.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
---
+++ src/main/java/com/ecom/repositories/PaymentRepository.java
@@ -0,0 +1,11 @@
+package com.ecom.repositories;
+
+import com.ecom.model.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long>{
+
+}
